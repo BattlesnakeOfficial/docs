0897b4a3399a22f2f8898eef164ba2051ffbfbd1
Andrew MacLean <andrewdmaclean@gmail.com>
2022-08-10 16:44:10 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome to the Official ${siteConfig.title}`}
+      description="A multiplayer programming game where your code is the controller. <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
