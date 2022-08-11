ed608ddadce7be8f76cee454e9308976f1ec8287
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2022-08-11 18:34:03 +0000

    Wrap buttons on homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,14 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <div className={styles.heroButtons}>
          <Link
-            className="button  button--secondary button--lg"
+            className={clsx('button button--secondary button--lg', styles.heroButton)}
             to="quickstart">
             Get Started
           </Link>
-
           <Link
-            className="button button--primary button--lg"
+            className={clsx('button button--primary button--lg', styles.heroButton)}
             to="https://play.battlesnake.com">
             Play Battlesnake
           </Link>
@@ -36,7 +35,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Welcome to the Battlesnake Docs`}
+      title="Welcome to the Battlesnake Docs"
       description="A multiplayer programming game where your code is the controller.">
       <HomepageHeader />
       <main>
