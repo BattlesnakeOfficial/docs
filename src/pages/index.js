9177915bd158034c737ab298aef9560a38e906a4
Andrew MacLean <andrewdmaclean@gmail.com>
2022-08-10 18:55:06 -0700

    Update Default Header Meta Tags (#12)
    * Update index.js
    
    * Add Link to Battlesnake Homepage
    
    This commit adds a link to https://play.battlesnake.com to the homepage
    
    * Fix Header Meta
    
    Fixing Issues from previous commit.
    
    * Add Margin to Button Group
    
    Fixes styling for the new button on the homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,16 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
+         <Link
+            className="button  button--secondary button--lg"
             to="quick-start">
             Get Started
+          </Link>
+
+          <Link
+            className="button button--primary button--lg"
+            to="https://play.battlesnake.com">
+            Play Battlesnake
           </Link>
         </div>
       </div>
@@ -30,8 +36,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome to the Battlesnake Docs`}
+      description="A multiplayer programming game where your code is the controller.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
