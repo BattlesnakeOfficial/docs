9bbcd16dbdd3bc7fbea98719abace020cbbb91ab
BrokenKeyboard <satasiyahet@gmail.com>
2022-08-28 18:59:21 +0000

    Update url to /docs/edit/main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/BattlesnakeOfficial/docs/tree/main',
+            'https://github.com/BattlesnakeOfficial/docs/edit/main',
         },
         blog: false,
         theme: {
