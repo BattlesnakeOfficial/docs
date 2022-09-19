cea822508d701880449162f5fcb28e036afaab7d
Rob O'Dwyer <odwyerrob@gmail.com>
2022-09-19 13:11:14 -0700

    add basic GA4 analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-ZLKCHQQP4Q',
+          anonymizeIP: true
+        }
       }),
     ],
   ],
