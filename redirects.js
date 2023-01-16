3043d8fc9057a24fd434e2c16a7e9a79edb05cdf
Rob O'Dwyer <odwyerrob@gmail.com>
2023-01-16 13:26:54 -0800

    fix bad redirects config

--- redirects.js
+++ redirects.js
@@ -1,21 +1,12 @@
 const redirects = [
-    {
-      from: '/guides/playing-battlesnake/battlegrounds',
-      to: '/guides/playing/leaderboards'
-    },
-    {
-      from: '/guides/playing-battlesnake/challenges',
-      to: '/guides/playing/challenges'
-    },
-    {
-      from: '/guides/playing-battlesnake/leagues',
-      to: '/guides/playing/leagues'
-    },
-    {
-      from: '/guides/playing/battlegrounds',
-      to: '/guides/playing/leaderboards'
-    }
-  ];
-  
-  module.exports = redirects;
-  
+  {
+    from: '/guides/playing-battlesnake/battlegrounds',
+    to: '/guides/playing/leaderboards'
+  },
+  {
+    from: '/guides/playing/battlegrounds',
+    to: '/guides/playing/leaderboards'
+  }
+];
+
+module.exports = redirects;
