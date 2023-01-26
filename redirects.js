0c099c514e956a5228525c29f8ea60a34eceb058
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2023-01-26 18:46:59 +0000

    Add redirect for legacy quickstart guide.

--- redirects.js
+++ redirects.js
@@ -1,16 +1,20 @@
 const redirects = [
   {
+    from: '/faq',
+    to: '/guides/faq'
+  },
+  {
+    from: '/guides/getting-started',
+    to: '/quickstart'
+  },
+  {
     from: '/guides/playing-battlesnake/battlegrounds',
     to: '/guides/playing/leaderboards'
   },
   {
     from: '/guides/playing/battlegrounds',
     to: '/guides/playing/leaderboards'
-  },
-  {
-    from: '/faq',
-    to: '/guides/faq'
-  }
+  }  
 ];
 
 module.exports = redirects;
