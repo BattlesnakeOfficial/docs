d2a7f1419b00afa0ecb717530a20b8d1162d27af
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2024-01-01 10:18:32 -0800

    Update announcement url.

--- src/announcement.js
+++ src/announcement.js
@@ -1,6 +1,6 @@
 const announcement = {
     id: 'announcement-2023-12-1',  // Increment on new announcement
-    content: '⭐️ Enjoying Battlesnake? <a href="/blog/2023/12/27/battlesnake-needs-your-support-in-2024">We need your help!</a> 💜',
+    content: '⭐️ Enjoying Battlesnake? <a href="https://github.com/orgs/BattlesnakeOfficial/discussions/247">We need your help!</a> 💜',
     backgroundColor: '#e70a77',
     textColor: '#ffffff',
     isCloseable: true
